ffc7b34b3b560db7e5f16b99391f9caf133c633b
VikaGor <Vica-gor@mail.ru>
2020-10-20 22:42:16 +0300

    Init

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "victoria_palindrome"
-
 require "minitest/autorun"
+require "minitest/reporters"
+Minitest::Reporters.use!
